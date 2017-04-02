308406b1bfbfa6dcfbb9f1028bada6f63071e6d0
NorthBei <seekingfor520@gmail.com>
2017-04-02 12:39:13 +0800

    Header style change to fit RWD
    1. Change the slick lib load from http to local
    2.Header Change to fit phone / tablet
    3.new product_intro page

---
+++ dev/product_intro/entry.js
@@ -0,0 +1,8 @@
+require("../_general/general.js");
+// $(document).ready(function(){
+//     $('.slick_slide').slick({
+//         dots: true,
+//         prevArrow:'<img src="./asset/Circled-Chevron-Left.png" id="slick-prev" class="slick-arrow">',
+//         nextArrow:'<img src="./asset/Circled-Chevron-Right.png" id="slick-next" class="slick-arrow">'
+//     });
+// });
