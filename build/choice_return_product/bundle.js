a04951847af02a39de78053daa01b27706ef5d71
NorthBei <seekingfor520@gmail.com>
2017-04-25 17:39:10 +0800

    Header finish half
    finish Header effect and layout of desktop

--- build/choice_return_product/bundle.js
+++ build/choice_return_product/bundle.js
@@ -97,6 +97,35 @@ $(document).ready(function() {
         }
         
     });
+
+    $(".parent_category_list>li").hover(function(){
+        var dropUp = $(this).find(".child_category_wrapper");
+        var dropUpWidth = dropUp.width();
+        var postion = dropUp.offset();//.toggleClass("");
+        var width = window.innerWidth;
+
+        if(postion == undefined || width <= 850){
+            return;
+        }
+        //console.log('x:'+postion.top+",y:"+postion.left);
+        
+        //console.log(postion.left);
+        if(postion.left+dropUpWidth > width){
+            $(this).addClass("child_will_attach");
+            dropUp.addClass("child_category_wrapper_attach_right");
+        }
+        else if(postion.left < 0){
+            $(this).addClass("child_will_attach");
+            dropUp.addClass("child_category_wrapper_attach_left");
+        }
+        
+        //child_category_wrapper
+    });
+
+    $("#bars").click(function(){
+        //$(".more_option")
+        console.log("x");
+    });
 });
 
 /***/ }),
