47bfdca7e0f5f752780b43f109e01519c8ac256d
NorthBei <seekingfor520@gmail.com>
2017-05-13 03:48:33 +0800

    Modify for match backend require
    Modify some js code or html code to meet backend requirement

--- dev/member_register/entry.js
+++ dev/member_register/entry.js
@@ -1,14 +1,94 @@
 require("../_general/general.js");
 
 window.addEventListener("load",function(){
+    var formdata = {};
+    // = {mail:regEmail,
+	// 						pass:regPass,
+	// 						name:regName,
+	// 						phone:regPhone,
+	// 						birth:birthArray};
     $("#email_register").on("click",function(){
-        $(".register_data_section").show();
-        $(".login_section").hide();
+        //At least one upper case english letter, (?=.*?[A-Z])
+        //At least one lower case english letter, (?=.*?[a-z])
+        //At least one digit, (?=.*?[0-9])
+        //At least one special character, (?=.*?[#?!@$%^&*-])
+        //Minimum 8 in length .{8,} (with the anchors)
+        var regexp = new RegExp("(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}");
+        var isEmailValid,isPasswordVaild,isPasswordSame = false;
+
+        var register_mail = $("#register_mail");
+        var mail = register_mail.val();
+        isEmailValid = validateEmail(mail);
+        //console.log("email"+isEmailValid);
+        judgeShowErrorMsg(register_mail,isEmailValid);
+
+        var register_password = $("#register_password");
+        var pass = register_password.val();
+        isPasswordVaild = regexp.test(pass);
+        //console.log("register_password "+isPasswordVaild);
+        judgeShowErrorMsg(register_password,isPasswordVaild);
+
+        var register_password_twice = $("#register_password_twice");
+
+        isPasswordSame = (register_password.val() == register_password_twice.val() ? true : false);
+        //console.log("register_password 2 "+isPasswordSame);
+        judgeShowErrorMsg(register_password_twice,isPasswordSame);
+
+        if(isEmailValid && isPasswordVaild && isPasswordSame){
+            formdata.mail = mail;
+            formdata.pass = pass;
+            
+            $(".register_data_section").show();
+            $(".login_section").hide();
+        }
+        
     });
 
     $("#check_register_button").on("click",function(){
-        $("#dialog p").css("visibility","visiable");
-        showDialog();
+        var isNameValid,isPhoneValid,isBirthValid = false;
+
+        var register_name = $("#register_name");
+        var name = register_name.val();
+        isNameValid = name == ""? false : true;
+        console.log("register_name "+isNameValid);
+        judgeShowErrorMsg(register_name,isNameValid);
+
+        var register_phone = $("#register_phone");
+        var phone = register_phone.val();
+        isPhoneValid = phone == ""? false : true;
+        console.log("register_phone "+isPhoneValid);
+        judgeShowErrorMsg(register_phone,isPhoneValid);
+
+        //Match entries that appear in the DD-MM-YYYY format
+        //Match entries that appear in the DD/MM/YYYY format
+        var $DD_MM_YYYY = "[0-9]{2}[-|\/]{1}[0-9]{2}[-|\/]{1}[0-9]{4}";
+        var regexp = new RegExp($DD_MM_YYYY);
+
+        var YYYY = $("#register_year").val();
+        var MM = $("#register_month").val();
+        var DD = $("#register_day").val();
+
+        var str = DD+"/"+MM+"/"+YYYY;
+
+        isBirthValid = regexp.test(str);
+        console.log(str+" = register_birth "+isBirthValid);
+        judgeShowErrorMsg($("#register_year").parent(),isBirthValid);
+
+        if(!isBirthValid){
+            $("#register_year").parent().next().addClass("marB32");
+            $(".register_data_section .reminder.marB32").hide();
+        }
+
+        if(isNameValid && isPhoneValid && isBirthValid){
+
+            formdata.name = name;
+            formdata.phone = phone;
+            formdata.birth = [DD,MM,YYYY];
+            
+            $("#dialog p").css("visibility","visiable");
+            showDialog();
+        }
+
     });
 
     $("#check_receive").on("click",function(){
@@ -18,4 +98,18 @@ window.addEventListener("load",function(){
 
 function showDialog(){
     $("#dialog").show().css('display', 'flex');
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function judgeShowErrorMsg(ele,isValid){
+    if(isValid){
+        ele.next().hide();
+    }
+    else{
+        ele.next().show();
+    }
 }
--- dev/order_query/entry.js
+++ dev/order_query/entry.js
@@ -1,10 +1,13 @@
 require("../_general/general.js");
 require("../_general/member_section/member_section.js");
 
+var data_orderID = "data-order_id";
 
 $(document).ready(function(){
     $(".do_cancel").click(function(){
-        $("#cancel_order_dialog").css("display","flex");
+        var orderID = $(this).parent().parent().find("td[data-th=訂單編號]").text().trim();
+        //console.log();
+        $("#cancel_order_dialog").css("display","flex").attr(data_orderID,orderID);
     });
 
     $(".do_return").click(function(){
@@ -19,15 +22,50 @@ $(document).ready(function(){
         }
     });
 
+    $("#user_error,#expensive,#more_like").click(function(){
+        document.querySelector(".cancel_reason").disabled = true;
+    });
+    $("#other").click(function(){
+        document.querySelector(".cancel_reason").disabled = false;
+    });
+
     $("#cancel_order_dialog .dialog_check").on("click",function(){
-        if($("#cancel_order_dialog .cancel_reason").val().length == 0){
-            $("#cancel_order_dialog .alter_msg").css('visibility', 'visible');
+        var label = $('input[name=cancel_type]:checked').next().text();
+        var cancel_note;
+        if(label == "其他"){
+            var text = $("#cancel_order_dialog .cancel_reason").val();
+            if( text.length == 0){
+                $("#cancel_order_dialog .alter_msg").css('visibility', 'visible');
+                return;
+            }
+            else{
+                $("#cancel_order_dialog .alter_msg").css('visibility', 'hidden');
+                cancel_note = text;
+            }
+
         }
         else{
-            //send to server
-            $("#cancel_order_dialog").hide();
-            $("#cancel_orde_success_dialog").css("display","flex");
+            cancel_note = label;
+            
+            //$("#cancel_order_dialog").hide();
+            //$("#cancel_orde_success_dialog").css("display","flex");
         }
+
+        var order_id = $("#cancel_order_dialog").attr(data_orderID);
+        //console.log(cancel_note,order_id);
+        //send to server
+        $.ajax({
+            type: "POST",
+            url: ajaxurl,
+            data: {"action":"cancel",
+                    "cancel_note":cancel_note,
+                    "order_id":order_id},
+            success: function(msg){
+                $("#cancel_order_dialog").hide();
+                $("#cancel_orde_success_dialog").css("display","flex");
+            }
+        });
+        
     });
     
 });
